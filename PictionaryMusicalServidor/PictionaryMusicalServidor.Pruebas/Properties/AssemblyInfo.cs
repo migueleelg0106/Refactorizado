7198ac77f1ef4f1f4b94ef439135dc5eb6c7c234
UrielCendon <urielcd2013@gmail.com>
2025-11-15 16:44:03 -0600

    Server pruebas

---
+++ PictionaryMusicalServidor/PictionaryMusicalServidor.Pruebas/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PictionaryMusicalServidor.Pruebas")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PictionaryMusicalServidor.Pruebas")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3b542c4-5eca-48c3-b8dd-9ca50d5806c3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
