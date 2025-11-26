764968a9ec1189d6318fe4f84b47ff53fe4d9e84
Miguel Eduardo Escobar Ladron De Guevara <miguelguevara850523@gmail.com>
2025-11-26 03:36:39 -0600

    Mostrar puntaje de jugadores al iniciar la partida

--- PictionaryMusicalCliente/Modelo/JugadorElemento.cs
+++ PictionaryMusicalCliente/Modelo/JugadorElemento.cs
@@ -13,6 +13,7 @@ namespace PictionaryMusicalCliente.Modelo
         private string _nombre;
         private bool _mostrarBotonExpulsar;
         private ICommand _expulsarComando;
+        private int _puntos;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -42,6 +43,15 @@ namespace PictionaryMusicalCliente.Modelo
         {
             get => _expulsarComando;
             set => EstablecerPropiedad(ref _expulsarComando, value);
+        }
+
+        /// <summary>
+        /// Puntaje acumulado del jugador durante la partida.
+        /// </summary>
+        public int Puntos
+        {
+            get => _puntos;
+            set => EstablecerPropiedad(ref _puntos, value);
         }
 
         private bool EstablecerPropiedad<T>(ref T campo, T valor, [CallerMemberName] string nombrePropiedad = null)
