dffcb51708195f1c9778f545475a46840da377ea
UrielCendon <urielcd2013@gmail.com>
2025-11-26 14:09:54 -0600

    Update SalaVistaModelo.cs

--- PictionaryMusicalCliente/VistaModelo/Salas/SalaVistaModelo.cs
+++ PictionaryMusicalCliente/VistaModelo/Salas/SalaVistaModelo.cs
@@ -117,6 +117,8 @@ namespace PictionaryMusicalCliente.VistaModelo.Salas
             _partidaVistaModelo = new PartidaIniciadaVistaModelo();
             _chatVistaModelo = new ChatVistaModelo();
 
+            _partidaVistaModelo.PropertyChanged += PartidaIniciadaVistaModelo_PropertyChanged;
+
             ConfigurarPartidaVistaModelo();
             ConfigurarChatVistaModelo();
 
@@ -174,6 +176,11 @@ namespace PictionaryMusicalCliente.VistaModelo.Salas
         private void ConfigurarChatVistaModelo()
         {
             _chatVistaModelo.EnviarMensajeAlServidor = EjecutarEnviarMensaje;
+        }
+
+        private void PartidaIniciadaVistaModelo_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            NotificarCambio(e.PropertyName);
         }
 
         private void OnJuegoIniciadoCambiado(bool juegoIniciado)
@@ -1364,6 +1371,8 @@ namespace PictionaryMusicalCliente.VistaModelo.Salas
         /// </summary>
         public async Task FinalizarAsync()
         {
+            _partidaVistaModelo.PropertyChanged -= PartidaIniciadaVistaModelo_PropertyChanged;
+
             _partidaVistaModelo.Detener();
 
             _salasServicio.JugadorSeUnio -= SalasServicio_JugadorSeUnio;
